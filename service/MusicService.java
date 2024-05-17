f7422d4f96078f494e8b1cca0f9c90d4eb497012
gbmahone <oliveiragabrielam@hotmail.com>
2024-05-17 18:37:02 +0100

    modifying menu

---
+++ service/MusicService.java
@@ -0,0 +1,14 @@
+package Spotify.service;
+
+import Spotify.model.Music;
+
+public class MusicService {
+
+    public MusicService(){}
+
+    public Music createMusic(){
+        Music music = new Music();
+        //cadastrar
+        return music;
+    }
+}
